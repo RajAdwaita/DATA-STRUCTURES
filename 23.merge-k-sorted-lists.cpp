08fe77f43b0870845bf03a384050bd287c535fdf
Adwaita Raj Modak <76047644+RajAdwaita@users.noreply.github.com>
2022-02-09 20:32:13 +0530

    Add files via upload

---
+++ 23.merge-k-sorted-lists.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=23 lang=cpp
+ *
+ * [23] Merge k Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+        if (lists.empty())
+        {
+            return nullptr;
+        }
+        while (lists.size() > 1)
+        {
+            lists.push_back(calc(lists[0], lists[1]));
+            lists.erase(lists.begin());
+            lists.erase(lists.begin());
+        }
+
+        return lists.front();
+    }
+
+    ListNode *calc(ListNode *l1, ListNode *l2)
+    {
+        if (l1 == NULL)
+            return l2;
+        if (l2 == NULL)
+
+            return l1;
+        if (l1->val <= l2->val)
+        {
+            l1->next = calc(l1->next, l2);
+            return l1;
+        }
+        else
+        {
+            l2->next = calc(l1, l2->next);
+            return l2;
+        }
+    }
+};
+// @lc code=end
---
+++ 4.median-of-two-sorted-arrays.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=4 lang=cpp
+ *
+ * [4] Median of Two Sorted Arrays
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+    {
+        
+    }
+};
+// @lc code=end
